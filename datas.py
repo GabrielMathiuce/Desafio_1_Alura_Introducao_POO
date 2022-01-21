0f0b8e16cccee0a37f6ea763a1d6e6864012a000
Gabriel <gabrmathiuce@gmail.com>
2022-01-21 14:26:11 -0300

    teste?:

---
+++ datas.py
@@ -0,0 +1,23 @@
+from email import message
+
+
+class Data:
+
+   def __init__(self, dia, mes, ano):
+     print("d.formatada(){}".format(self))
+     self.data = {dia, mes, ano}
+     self.dia = dia
+     self.mes = mes 
+     self.ano = ano
+     self.formatada
+
+   def formatada(self):
+     print("{} {} {}".format(self.dia, self.mes, self.ano))
+     
+
+
+
+  
+     
+
+
